344162c8d46d54c0877725e05080952dee8792f7
Renzo Jorge <renzoch99@gmail.com>
2026-07-16 17:34:45 -0500

    feat(i18n): switch language client-side without page navigation

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,4 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({
-  i18n: {
-    locales: ['es', 'en'],
-    defaultLocale: 'es',
-    routing: {
-      prefixDefaultLocale: false,
-    },
-  },
-});
+export default defineConfig({});
